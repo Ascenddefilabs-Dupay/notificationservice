5610f769e30268c9afd7b5885cf1d5259c08e95f
DinakarY7 <dinakar.yaramati@ascenddefi.com>
2024-09-18 16:46:09 +0530

    Made changes in the Notifications

--- backend-notificationservice/notifications/models.py
+++ backend-notificationservice/notifications/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Transaction(models.Model):
+    transaction_id = models.AutoField(primary_key=True)  # Explicitly declare the primary key
+    wallet_id = models.CharField(max_length=50)
+    transaction_type = models.CharField(max_length=50)
+    transaction_currency = models.CharField(max_length=50)
+    transaction_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_timestamp = models.DateTimeField()
+    transaction_status = models.CharField(max_length=50)
+    user_phone_number = models.CharField(max_length=20)
+    sender_mobile_number = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'transaction_table'
+        managed = False
